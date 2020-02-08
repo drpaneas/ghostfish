5ac83fb09eafd49d4c09da26cba5955d2352416d
Panagiotis Georgiadis <pgeorgia@redhat.com>
2020-02-08 19:56:20 +0100

    Scan all 1024 ports concurrently
    Use go routines
    
    * This commit is introducing a bug

--- main.go
+++ main.go
@@ -7,14 +7,17 @@ import (
 
 func main() {
 	for i := 1; i <= 1024; i++ {
-		address := fmt.Sprintf("scanme.nmap.org:%d", i)
-		conn, err := net.Dial("tcp", address)
-		if err != nil {
-			// port is closed or filtered
-			continue
-		}
-		// Connection is successful
-		conn.Close() // FINishing the connection is just polite
-		fmt.Printf("TCP/%d open\n", i)
+		go func(j int) {
+
+			address := fmt.Sprintf("scanme.nmap.org:%d", j)
+			conn, err := net.Dial("tcp", address)
+			if err != nil {
+				// port is closed or filtered
+				return
+			}
+			// Connection is successful
+			conn.Close() // FINishing the connection is just polite
+			fmt.Printf("TCP/%d open\n", j)
+		}(i)
 	}
 }
