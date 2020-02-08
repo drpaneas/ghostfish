22ff5da3489bbb05a0b9fde62cf9d1a2ce0e6ac1
Panagiotis Georgiadis <pgeorgia@redhat.com>
2020-02-08 19:41:24 +0100

    Scan all 1024 ports in sequence
    Implement a 'for' loop to scan all the 1024 ports
    on by one.
    
    For the ports that open, close the established connection with
    the remote server by sending a 'FIN' packet. That is done
    by using the 'conn.Close()' function. It is not needed per se
    but it is polite to do it.

--- main.go
+++ main.go
@@ -6,8 +6,15 @@ import (
 )
 
 func main() {
-	_, err := net.Dial("tcp", "scanme.nmap.org:80")
-	if err == nil {
-		fmt.Println("Connection successful")
+	for i := 1; i <= 1024; i++ {
+		address := fmt.Sprintf("scanme.nmap.org:%d", i)
+		conn, err := net.Dial("tcp", address)
+		if err != nil {
+			// port is closed or filtered
+			continue
+		}
+		// Connection is successful
+		conn.Close() // FINishing the connection is just polite
+		fmt.Printf("TCP/%d open\n", i)
 	}
 }
